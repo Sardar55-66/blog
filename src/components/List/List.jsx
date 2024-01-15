d3461e41600068a7bc190323c4071a62d2383900
Sardar55-66 <104762151+Sardar55-66@users.noreply.github.com>
2024-01-15 16:16:37 +0300

    lastexact

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -7,31 +7,32 @@ import { getArticle } from '../api/get-api-data';
 
 function ListOfArticles(props) {
   const dispatch = useDispatch();
-  const article = props.data;
-  const { author } = article;
+  const articles = props.data.slice(0, 5);
 
   return (
-    <div className="article article_fs">
-      <div className="article__header">
-        <div className="article__title">
-          <Link className="title-link" onClick={() => dispatch(getArticle(article.slug))} to={`/articles/${article.slug}`}>{article.title}</Link>
-          <span className="article__likes">
-            <img className="heart" src={heart} alt="heart" />
-            <span className="like-counter">{article.favoritesCount}</span>
-          </span>
+    articles.map((article) => (
+      <div className="article article_fs">
+        <div className="article__header">
+          <div className="article__title">
+            <Link className="title-link" onClick={() => dispatch(getArticle(article.slug))} to={`/articles/${article.slug}`}>{article.title}</Link>
+            <span className="article__likes">
+              <img className="heart" src={heart} alt="heart" />
+              <span className="like-counter">{article.favoritesCount}</span>
+            </span>
+          </div>
+          <div className="article__author">
+            <span className="author_name">{article.author.username}</span>
+            <span className="author__avatar">
+              <img className="avatar-image" src={article.author.image} alt="avatar" />
+            </span>
+            <span className="article_date">{article.createdAt}</span>
+          </div>
         </div>
-        <div className="article__author">
-          <span className="author_name">{author.username}</span>
-          <span className="author__avatar">
-            <img className="avatar-image" src={author.image} alt="avatar" />
-          </span>
-          <span className="article_date">{article.createdAt}</span>
-        </div>
+        <div className="article__tag">{article.tagList.length !== 0 ? article.tagList : 'some tag'}</div>
+        <div className="article__tag">some tag</div>
+        <span className="article__text">{article.description}</span>
       </div>
-      <div className="article__tag">{article.tagList.length !== 0 ? article.tagList : 'some tag'}</div>
-      <div className="article__tag">some tag</div>
-      <span className="article__text">{article.description}</span>
-    </div>
+    ))
   );
 }
 
--- src/components/Reducers/Reducers.jsx
+++ src/components/Reducers/Reducers.jsx
@@ -13,7 +13,7 @@ export const addArticlesList = (state = initialStateForArticles, action) => {
         isLoaded: true,
         start: state.start + 5,
         end: state.end + 5,
-        articles: [...action.payload],
+        articles: [action.payload],
       };
 
     default:
