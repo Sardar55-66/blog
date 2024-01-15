2a7cd9d2362cd1757e663cc1b08e3b13ea0ea0f8
Sardar55-66 <104762151+Sardar55-66@users.noreply.github.com>
2024-01-15 16:02:45 +0300

    last

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -83,7 +83,7 @@ function App() {
       </header>
 
       <Routes>
-        {/* <Route path="/" element={articles.length === 0 ? <NoArticle /> : null} /> */}
+        <Route path="/" element={articles.length === 0 ? <NoArticle /> : null} />
         <Route path="/authorized-list" element={articles.length === 0 ? <NoArticle /> : null} />
         <Route path="/authorized-list/:profile?" element={<EditProfile />} />
         <Route path="/authorized-list/new-article" element={<CreateArticle />} />
